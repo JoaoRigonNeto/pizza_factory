c3728c3a624d4bc7e3c86d3e0074263b58240548
Joao Alceu Rigon Neto <joao@Joaos-MacBook-Air.local>
2022-11-17 23:42:00 -0300

    Adicionando sabores novos

--- kitchen/margherita.py
+++ kitchen/margherita.py
@@ -5,17 +5,11 @@ class Margherita(Pizza):
 
     def __init__(self):
         super().__init__()
-        self.basic_ingredients = ["cheese", "tomato", "bibi`s special sauce"]
-        self.removable_ingredients = ["tomato"]
-        self.addable_ingredients = ["bibi`s special sauce"]
+        self.ingredients = ["tomato sauce", "mozzarella cheese", "parmegiano-reggiano cheese", "basil", "red pepper"]
     
     def is_delicious(self):
         return True
 
     def remove_ingredient(self, ingredient: str):
-        if ingredient.lower() in self.basic_ingredients and ingredient.lower() in self.removable_ingredients:
-            self.basic_ingredients.remove(ingredient.lower())
-        
-    def add_ingredient(self, ingredient: str):
-        if ingredient.lower() in self.basic_ingredients and ingredient.lower() in self.addable_ingredients:
-            self.basic_ingredients.append(ingredient.lower())
+        if ingredient.lower() in self.ingredients:
+            self.ingredients.remove(ingredient.lower())
